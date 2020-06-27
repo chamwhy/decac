b357f95ec1855209ee2cadb396ebc737e6fae8ec
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-06-27 23:18:18 +0900

    add

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public CamMove cam;
+    public GameObject player;
+
+    public bool isStart = false;
+    int score;
+    int bestScore;
+
+    public Animator[] startMenuAnim;
+    public Animator[] endMenuAnim;
+    public Animator[] changeMuneAnim;
+    public Animator[] touchAnim;
+    public Animator scoreAnim;
+
+    private IEnumerator coroutine;
+    public void StartBtn(){
+        AnimTrigger("off", 0);
+        AnimTrigger("on", 3);
+        StartGame();
+    }
+
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        isStart = true;
+        score = 0;
+        coroutine = AddScore();
+        StartCoroutine(coroutine);
+    }
+
+    public void EndGame()
+    {
+        isStart = false;
+        StopCoroutine(coroutine);
+        cam.CloseUp();
+        Invoke("EndAnim", 0.8f);
+    }
+
+    IEnumerator AddScore()
+    {
+        while (true)
+        {
+            score++;
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+    /* plus functions */
+
+    private void EndAnim()
+    {
+        AnimTrigger("on", 1);
+    }
+
+
+    /* Game functions */
+    public void AnimTrigger(string trigger, int type)
+    {
+        switch (type)
+        {
+            case 0:
+                Trigger(startMenuAnim, trigger);
+                break;
+            case 1:
+                Trigger(endMenuAnim, trigger);
+                break;
+            case 2:
+                Trigger(changeMuneAnim, trigger);
+                break;
+            case 3:
+                scoreAnim.SetTrigger(trigger);
+                break;
+        }
+    }
+
+    void Trigger(Animator[] anim, string trigger)
+    {
+        for (int i = 0; i < anim.Length; i++)
+        {
+            anim[i].SetTrigger(trigger);
+        }
+    }
+
+
+    public void TouchEffect(bool isRight)
+    {
+        if (isStart)
+        {
+            touchAnim[Convert.ToInt32(isRight)].SetTrigger("touch");
+        }
+    }
+
+
+    /* buttons' event */
+
+    
+
+    public void PauseBtn(){
+
+    }
+
+    public void RestartBtn(){
+
+    }
+
+    public void HomeBtn(){
+
+    }
+
+    public void ChangeBtn(){
+
+    }
+
+    public void ArrowBtn(bool isRight){
+
+    }
+
+    public void SellectBtn(){
+
+    }
+}
+
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public Rigidbody2D rigid;
+
+    public float spped = 5f;
+    public float jumpForce = 10f;
+    private bool isGround = false;
+    private int jumpCount = 0;
+    private int maxJumpCount = 1;
+    
+    private void Update()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, spped);
+        int which = (transform.position.x > 0) ? 2 : -2;
+        if (rigid.gravityScale != which) rigid.gravityScale = which;
+
+        if (gameManager.isStart)
+        {
+            if (Input.touchCount > 0)
+            {
+                Debug.Log("click");
+                for(int i = 0; i < Input.touches.Length; i++)
+                {
+                    switch (Input.touches[i].phase)
+                    {
+                        case TouchPhase.Began:
+                            if (jumpCount < maxJumpCount)
+                            {
+                                rigid.AddForce(new Vector2(jumpForce * which, 0));
+                                gameManager.TouchEffect(Convert.ToBoolean(which+2));
+                            }
+                            break;
+                        case TouchPhase.Ended:
+
+                            break;
+                        case TouchPhase.Stationary:
+
+                            break;
+                        case TouchPhase.Moved:
+
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
